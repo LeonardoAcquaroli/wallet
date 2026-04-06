2c51c43ba6b0e55601f34a3f67e5217480e98f70
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2026-04-06 18:50:38 +0200

    overwrite values when re-launched monthly report

--- config/queries.py
+++ config/queries.py
@@ -2,6 +2,12 @@
 INSERT_MONTHLY_FINANCIAL_UPDATE = """
     INSERT INTO monthly_financial_update (ref_month, fund_id, amount, details)
     VALUES (%s, %s, %s, %s)
+    ON CONFLICT (ref_month, fund_id)
+    DO UPDATE SET
+        amount = EXCLUDED.amount,
+        details = EXCLUDED.details,
+        updated_at = NOW()
+    WHERE EXCLUDED.updated_at > monthly_financial_update.updated_at
 """
 
 SELECT_FUNDS = "SELECT fund_id, fund_name FROM funds"
@@ -17,4 +23,9 @@ GET_PREVIOUS_SALDO = """
 INSERT_SALDO = """
     INSERT INTO saldo (ref_month, amount)
     VALUES (%s, %s)
+    ON CONFLICT (ref_month)
+    DO UPDATE SET
+        amount = EXCLUDED.amount,
+        updated_at = NOW()
+    WHERE EXCLUDED.updated_at > saldo.updated_at
 """
